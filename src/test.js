390ce09ee0fa18eec87c31ff882f21629bbedb5e
Marshall3572 <944797458@qq.com>
2021-03-23 21:06:05 +0800

    添加了跳动的代码，动态显示画皮卡丘的过程

---
+++ src/test.js
@@ -0,0 +1,77 @@
+import string from './css.js'
+
+
+const player = {
+  id: undefined,
+  time: 100,
+  ui: {
+    demo: document.querySelector('#demo'),
+    demo2: document.querySelector('#demo2')
+  },
+  events: {
+    '#btnPause': 'pause',
+    '#btnPlay': 'play',
+    '#btnSlow': 'slow',
+    '#btnNormal': 'normal',
+    '#btnFast': 'fast'
+  },
+  n: 1,
+  init: () => {
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.bindEvents()
+    player.play()
+  },
+  bindEvents: () => {
+    for (let key in player.events) {
+      if (player.events.hasOwnProperty(key)) {
+        const value = player.events[key] // pause / play / slow
+        document.querySelector(key).onclick = player[value]
+      }
+    }
+
+  },
+  run: () => {
+    player.n += 1
+    if (player.n > string.length) {
+      window.clearInterval(player.id)
+      return
+    }
+    // 在页面展示代码
+    player.ui.demo.innerText = string.substr(0, player.n)
+    // 在HTMl中插入代码，产生效果
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    // 将滚动条自动向下滚动的高度设置为可滚动的高度
+    player.ui.demo.scrollTop = player.ui.demo.scrollHeight
+  },
+  play: () => {
+    window.clearInterval(player.id)
+    player.id = setInterval(player.run, player.time)
+  },
+  pause: () => {
+    window.clearInterval(player.id)
+  },
+  slow: () => {
+    player.pause()
+    player.time = 300
+    player.play()
+  },
+  normal: () => {
+    player.pause()
+    player.time = 100
+    player.play()
+  },
+  fast: () => {
+    player.pause()
+    player.time = 0
+    player.play()
+  }
+}
+
+player.init()
+
+
+
+
+
+
